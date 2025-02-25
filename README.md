c31dc1b2df2c71297b5ede096bc5b208a3c37f01
Michael Naish <mnaish@uwo.ca>
2025-02-25 14:58:31 -0500

    Update for MSEduino R6.1

--- README.md
+++ README.md
@@ -1,24 +1,16 @@
 # TCS34725 Demo
 
-This demo connects to a TCS34725 colour sensor over the I2C bus and allows the measured colour components to be observed using the Serial Monitor. If working properly, the RBGC values will vary when objects of different colours are placed in close proximity to the sensor. SW0 (S1-2) may be used to turn the illumination LEDs on the TCS34725 on and off.
+This demo connects to a TCS34725 colour sensor over the I2C bus and allows the measured colour components to be observed using the Serial Monitor. If working properly, the RBGC values will vary when objects of different colours are placed in close proximity to the sensor. SW1 (S1-2) may be used to turn the illumination LEDs on the TCS34725 on and off.
 
 ## Hardware Required
 
 For this demo, you will require the following components:
 
-* MSEduino ESP32-S3 based development board
+* MSEduino ESP32-based development board
 * TCS34725 colour sensor
-* Jumper or Dupont wires (M–F) to connect components
+* Jumper or Dupont wires (–F) to connect components
 
 ![TCS34725 Demo Schematic](docs/TCS34725_schematic.png)
-
-### Build and Flash
-
-Open the [Arduino IDE](https://www.arduino.cc/en/software) and configure it for the ESP32. Use a USB-A extension cable to connect your MSEduino to your computer. Use the **Tools→Board→esp32** menu to select the **Adafruit Feather ESP32-S3 No PRAM board**. The Port should be set as **COMx** on Windows or **dev.cu.usbmodel14101** (or similar) on Mac. The code provided with this lab requires version 2.0.11 of the Arduino-ESP32 boards. Use the **Tools→Board→Boards Manager...** menu and find the **esp32 by Espressif** board package. Confirm that the version is **2.0.11**. If necessary, use the dropdown menu to change versions. The code will not work in versions 3.0+ due to changes made to the underlying esp-idf API.
-
-#### VSCode and PlatformIO
-
-Note that as an alternative to the Arduino IDE, this project can be developed using [VSCode](https://code.visualstudio.com) with the [PlatformIO IDE](https://platformio.org/platformio-ide) extension and the [Espressif 32 platform](https://registry.platformio.org/platforms/platformio/espressif32) installed.
 
 ### Library Installation
 
@@ -33,8 +25,6 @@ The [Adafruit NeoPixel Library](https://github.com/adafruit/Adafruit_NeoPixel) i
 The [Adafruit TCS34725 Library](https://www.arduino.cc/reference/en/libraries/adafruit-tcs34725/) is another third-party library that can be installed from the Ardinio IDE Library Manager. Go to **Tools→Manage Libraries**. Enter "tcs34725" in the filter textbox. Scroll to find the "**Adafruit TCS34725** by Adafruit" library. Ensure that the latest version (≥ 1.4.4) is selected and press the **INSTALL** button. You will be asked whether you also want to install the dependency **Adafruit BusIO**. Select **INSTALL ALL**. 
 
 ![Install Adafruit TCS34725 Library](docs/Install_TCS34725.png)
-
-For users of VSCode/PlatformIO, dependancies for the Adafruit libraries are contained in the `platformio.ini` configuration file. These will automatically install the libraries, if necessary.
 
 ## Resources
 
--- TCS34725_Demo/TCS34725_Demo.ino
+++ TCS34725_Demo/TCS34725_Demo.ino
@@ -2,12 +2,12 @@
 // MSE 2202 TCS34725 colour sensor example
 // 
 //  Language: Arduino (C++)
-//  Target:   ESP32-S3
+//  Target:   ESP32
 //  Author:   Michael Naish
-//  Date:     2024 03 05 
+//  Date:     2025 02 25 
 //
 
-#define PRINT_COLOUR                                  // uncomment to turn on output of colour sensor data
+#define PRINT_COLOUR                                                   // uncomment to turn on output of colour sensor data
 
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
@@ -19,19 +19,19 @@
 void doHeartbeat();
 
 // Constants
-const int cHeartbeatInterval = 75;                    // heartbeat update interval, in milliseconds
-const int cSmartLED          = 21;                    // when DIP switch S1-4 is on, SMART LED is connected to GPIO21
-const int cSmartLEDCount     = 1;                     // number of Smart LEDs in use
-const int cSDA               = 47;                    // GPIO pin for I2C data
-const int cSCL               = 48;                    // GPIO pin for I2C clock
-const int cTCSLED            = 14;                    // GPIO pin for LED on TCS34725
-const int cLEDSwitch         = 46;                    // DIP switch S1-2 controls LED on TCS32725    
+const int cHeartbeatInterval = 75;                                     // heartbeat update interval, in milliseconds
+const int cSmartLED          = 21;                                     // when DIP switch S1-4 is on, SMART LED is connected to GPIO21
+const int cSmartLEDCount     = 1;                                      // number of Smart LEDs in use
+const int cSDA               = 18;                                     // GPIO pin for I2C data
+const int cSCL               = 19;                                     // GPIO pin for I2C clock
+const int cTCSLED            = 14;                                     // GPIO pin for LED on TCS34725
+const int cLEDSwitch         = 34;                                     // DIP switch S1-2 controls LED on TCS32725    
 
 // Variables
-boolean heartbeatState       = true;                  // state of heartbeat LED
-unsigned long lastHeartbeat  = 0;                     // time of last heartbeat state change
-unsigned long curMillis      = 0;                     // current time, in milliseconds
-unsigned long prevMillis     = 0;                     // start time for delay cycle, in milliseconds
+boolean heartbeatState       = true;                                   // state of heartbeat LED
+unsigned long lastHeartbeat  = 0;                                      // time of last heartbeat state change
+unsigned long curMillis      = 0;                                      // current time, in milliseconds
+unsigned long prevMillis     = 0;                                      // start time for delay cycle, in milliseconds
 
 // Declare SK6812 SMART LED object
 //   Argument 1 = Number of LEDs (pixels) in use
@@ -52,21 +52,21 @@ unsigned char LEDBrightnessLevels[] = {0, 0, 0, 5, 15, 30, 45, 60, 75, 90, 105, 
 // TCS34725 colour sensor with 2.4 ms integration time and gain of 4
 // see https://github.com/adafruit/Adafruit_TCS34725/blob/master/Adafruit_TCS34725.h for all possible values
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_4X);
-bool tcsFlag = 0;                                     // TCS34725 flag: 1 = connected; 0 = not found
+bool tcsFlag = 0;                                                      // TCS34725 flag: 1 = connected; 0 = not found
 
 void setup() {
-  Serial.begin(115200);                               // Standard baud rate for ESP32 serial monitor
+  Serial.begin(115200);                                                // Standard baud rate for ESP32 serial monitor
 
   // Set up SmartLED
-  SmartLEDs.begin();                                  // initialize smart LEDs object
-  SmartLEDs.clear();                                  // clear pixel
-  SmartLEDs.setPixelColor(0, SmartLEDs.Color(0,0,0)); // set pixel colours to black (off)
-  SmartLEDs.setBrightness(0);                         // set brightness [0-255]
-  SmartLEDs.show();                                   // update LED
-
-  Wire.setPins(cSDA, cSCL);                           // set I2C pins for TCS34725
-  pinMode(cTCSLED, OUTPUT);                           // configure GPIO to control LED on TCS34725
-  pinMode(cLEDSwitch, INPUT_PULLUP);                  // configure GPIO to set state of TCS34725 LED 
+  SmartLEDs.begin();                                                   // initialize smart LEDs object
+  SmartLEDs.clear();                                                   // clear pixel
+  SmartLEDs.setPixelColor(0, SmartLEDs.Color(0,0,0));                  // set pixel colours to black (off)
+  SmartLEDs.setBrightness(0);                                          // set brightness [0-255]
+  SmartLEDs.show();                                                    // update LED
+                 
+  Wire.setPins(cSDA, cSCL);                                            // set I2C pins for TCS34725
+  pinMode(cTCSLED, OUTPUT);                                            // configure GPIO to control LED on TCS34725
+  pinMode(cLEDSwitch, INPUT_PULLUP);                                   // configure GPIO to set state of TCS34725 LED 
 
   // Connect to TCS34725 colour sensor
   if (tcs.begin()) {
@@ -80,30 +80,30 @@ void setup() {
 }
 
 void loop() {
-  uint16_t r, g, b, c;                                // RGBC values from TCS34725
+  uint16_t r, g, b, c;                                                 // RGBC values from TCS34725
   
-  digitalWrite(cTCSLED, !digitalRead(cLEDSwitch));    // turn on onboard LED if switch state is low (on position)
-  if (tcsFlag) {                                      // if colour sensor initialized
-    tcs.getRawData(&r, &g, &b, &c);                   // get raw RGBC values
+  digitalWrite(cTCSLED, !digitalRead(cLEDSwitch));                     // turn on onboard LED if switch state is low (on position)
+  if (tcsFlag) {                                                       // if colour sensor initialized
+    tcs.getRawData(&r, &g, &b, &c);                                    // get raw RGBC values
 #ifdef PRINT_COLOUR            
       Serial.printf("R: %d, G: %d, B: %d, C %d\n", r, g, b, c);
 #endif
   }
-  doHeartbeat();                                      // update heartbeat LED
+  doHeartbeat();                                                       // update heartbeat LED
 }
 
 // update heartbeat LED
 void doHeartbeat() {
-  curMillis = millis();                               // get the current time in milliseconds
+  curMillis = millis();                                                // get the current time in milliseconds
   // check to see if elapsed time matches the heartbeat interval
   if ((curMillis - lastHeartbeat) > cHeartbeatInterval) {
-    lastHeartbeat = curMillis;                        // update the heartbeat time for the next update
-    LEDBrightnessIndex++;                             // shift to the next brightness level
-    if (LEDBrightnessIndex > sizeof(LEDBrightnessLevels)) { // if all defined levels have been used
-      LEDBrightnessIndex = 0;                         // reset to starting brightness
+    lastHeartbeat = curMillis;                                         // update the heartbeat time for the next update
+    LEDBrightnessIndex++;                                              // shift to the next brightness level
+    if (LEDBrightnessIndex > sizeof(LEDBrightnessLevels)) {            // if all defined levels have been used
+      LEDBrightnessIndex = 0;                                          // reset to starting brightness
     }
-    SmartLEDs.setBrightness(LEDBrightnessLevels[LEDBrightnessIndex]); // set brightness of heartbeat LED
-    SmartLEDs.setPixelColor(0, SmartLEDs.Color(0, 250, 0)); // set pixel colours to green
-    SmartLEDs.show();                                 // update LED
+    SmartLEDs.setBrightness(LEDBrightnessLevels[LEDBrightnessIndex]);  // set brightness of heartbeat LED
+    SmartLEDs.setPixelColor(0, SmartLEDs.Color(0, 250, 0));            // set pixel colours to green
+    SmartLEDs.show();                                                  // update LED
   }
 }
